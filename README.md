0126e9f4e12210a95eba61a15a3116344571897a
Mike Ngu <mikengu01@gmail.com>
2020-08-26 08:45:46 -0500

    feat: add tools

--- README.md
+++
@@ -1,2 +0,0 @@
-### Hello 👋 Welcome to my Github Page. I am just your average programmer that enjoys helping others through code. Feel free to reach me [here](https://www.linkedin.com/in/mikengu/) if you like to chat or connect 🙂
-This was last updated on 26 Aug 2020.
--- src/main.go
+++ src/main.go
@@ -8,11 +8,22 @@ import (
 )
 
 func makeReadme(filename string) error {
+	react:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png\"></code>"
+	javascript:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png\"></code>"
+	typescript:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png\"></code>"
+	node:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png\"></code>"
+	graphql:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/5c058a388828bb5fde0bcafd4bc867b5bb3f26f3/topics/graphql/graphql.png\"></code>"
+	python:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png\"></code>"
+	goLang:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/go/go.png\"></code>"
+	vim:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vim/vim.png\"></code>"
+	docker:= "<code><img height=\"20\" src=\"https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/docker/docker.png\"></code>"
 	date := time.Now().Format("2 Jan 2006")
 	intro := "### Hello 👋 Welcome to my Github Page. I am just your average programmer that enjoys helping others through code. Feel free to reach me [here](https://www.linkedin.com/in/mikengu/) if you like to chat or connect 🙂"
+	tools :=  "### Languages and Tools I use: \n" + react + " " + javascript + " " + typescript + " " + node + " " + graphql + " " + python + " " + goLang + " " + vim + " " + docker 
+
 	updateDate := "This was last updated on " + date + "."
 
-	data := fmt.Sprintf("%s\n%s\n", intro, updateDate)
+	data := fmt.Sprintf("%s\n%s\n\n%s\n", intro, tools, updateDate)
 
 	file, err := os.Create(filename)
 	if err != nil {
