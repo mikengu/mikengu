da7fdc5fd07a124855f03677353fed4c6c812806
Mike Ngu <mikengu01@gmail.com>
2020-08-25 20:29:19 -0500

    chore: update

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 
 func makeReadme(filename string) error {
 	date := time.Now().Format("2 Jan 2006")
-	intro := "### Hello 👋 Welcome to my Github Page. I am just your average programmer that enjoys helping others through code. Feel free to reach me [here](https://www.linkedin.com/in/mikengu/) if you like to chat 🙂"
+	intro := "### Hello 👋 Welcome to my Github Page. I am just your average programmer that enjoys helping others through code. Feel free to reach me [here](https://www.linkedin.com/in/mikengu/) if you like to chat or connect 🙂"
 	updateDate := "This was last updated on " + date + "."
 
 	data := fmt.Sprintf("%s\n%s\n", intro, updateDate)
